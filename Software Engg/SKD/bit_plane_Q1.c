daac9505241ffeabc5f844c632c1e6a3d28506b0
Soumitri Chattopadhyay <56516406+soumitri2001@users.noreply.github.com>
2021-04-11 02:50:09 +0530

    Update bit_plane_Q1.c

--- Software Engg/SKD/bit_plane_Q1.c
+++ Software Engg/SKD/bit_plane_Q1.c
@@ -12,7 +12,6 @@ const int NUM_BITS = 8;
 
 int **initializeMatrix(int, int);
 void printPlane(int **, int, int, int);
-int bitPlaneValue(int, int);
 
 int main(int argc, char **argv)
 {
@@ -33,18 +32,18 @@ int main(int argc, char **argv)
         }
     }
 
-    int p;
-    for (p = 0; p < NUM_BITS; p++)
+    int pIndex;
+    for (pIndex = 0; pIndex < NUM_BITS; pIndex++)
     {
         int **plane = initializeMatrix(rows, cols);
         for (i = 0; i < rows; i++)
         {
             for (j = 0; j < cols; j++)
             {
-                plane[i][j] = bitPlaneValue(mat[i][j], p);
+                plane[i][j] = (mat[i][j] & (1 << pIndex));
             }
         }
-        printPlane(plane, rows, cols, p);
+        printPlane(plane, rows, cols, pIndex);
         printf("-----------------------------------------\n");
     }
     return 0;
@@ -59,18 +58,6 @@ int **initializeMatrix(int rows, int cols)
         mat[i] = (int *)malloc(cols * sizeof(int *));
     }
     return mat;
-}
-
-int bitPlaneValue(int num, int pIndex)
-{
-    int itr = pIndex;
-    while (itr > 0)
-    {
-        num >>= 1;
-        itr--;
-    }
-    int bit = (num & 1);
-    return bit * (1 << pIndex);
 }
 
 void printPlane(int **plane, int rows, int cols, int pIndex)
