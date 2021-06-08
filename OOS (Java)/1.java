a5b53dfceb6f44174aed012d023da201f87089dd
soumitri2001 <56516406+soumitri2001@users.noreply.github.com>
2021-06-09 02:24:35 +0530

    OOS complete upto 25

--- OOS (Java)/1.java
+++ OOS (Java)/1.java
@@ -1,7 +1,9 @@
-/* 1) Create a class “Room” which will hold the “height”, “width” and “breadth” of the room in three fields. 
+/* 
+1) Create a class “Room” which will hold the “height”, “width” and “breadth” of the room in three fields. 
 This class also has a method “volume()” to calculate the volume of this room. 
 Create another class “RoomDemo” which will use the earlier class, create instances of rooms, 
-and display the volume of room */
+and display the volume of room 
+*/
 
 class Room {
 
